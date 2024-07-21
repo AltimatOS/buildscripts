0037811d950b073f595471bd757b33783fcdf337
AltimatOS Build System <builds@altimatos.org>
2024-07-20 22:32:17 -0400

    Add autoconf
    Signed-off-by: AltimatOS Build System <builds@altimatos.org>

---
+++ pkgs/26-autoconf/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+set -x
+
+function compress_manpages {
+    local section=${1}
+    for FILE in $(find /System/share/man/man${section} -type f -name "*.${section}"); do
+        echo "Compressing file: $FILE"
+        gzip -f -9 $FILE
+    done
+}
+
+NAME=autoconf
+VERSION=2.72
+SOURCE0="${NAME}-${VERSION}.tar.xz"
+SRC_DIR="${NAME}-${VERSION}"
+
+if [[ "$(pwd)" != "/srcs" ]]; then
+    cd /srcs
+fi
+
+# now unpack
+tar xvf $SOURCE0
+
+pushd $SRC_DIR
+    ./configure --prefix=/System
+    make
+    make check
+    make install
+popd
+
+compress_manpages 1
+
+# clean up
+rm -rf $SRC_DIR
