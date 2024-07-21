25b4186233a7638f343e983eecfd2a7d7741d104
AltimatOS Build System <builds@altimatos.org>
2024-07-21 12:23:09 -0400

    Add 32bit build for the Linux ACL library
    Signed-off-by: AltimatOS Build System <builds@altimatos.org>

---
+++ pkgs/39-acl-32bit/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+set -x
+
+function compress_manpages {
+    local section=${1}
+    for FILE in $(find /System/share/man/man${section} -type f -name "*.${section}"); do
+        echo "Compressing file: $FILE"
+        gzip -f -9 $FILE
+    done
+}
+
+NAME=acl
+VERSION=2.3.2
+SOURCE0="${NAME}-${VERSION}.tar.xz"
+SRC_DIR="${NAME}-${VERSION}"
+
+if [[ "$(pwd)" != "/srcs" ]]; then
+    cd /srcs
+fi
+
+# now unpack
+tar xvf $SOURCE0
+
+pushd $SRC_DIR
+    CC="gcc -m32" ./configure    \
+        --prefix=/System         \
+        --libdir=/System/lib     \
+        --libexecdir=/System/lib \
+        --host=i686-pc-linux-gnu
+    make
+    make DESTDIR=$PWD/DESTDIR install
+    cp -Rv DESTDIR/System/lib/* /System/lib
+    rm -rf DESTDIR
+popd
+
+# clean up
+rm -rf $SRC_DIR
