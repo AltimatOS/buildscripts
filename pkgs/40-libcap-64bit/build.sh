b862f89384f522bbe8fca4237f140056a7014bbc
AltimatOS Build System <builds@altimatos.org>
2024-07-21 12:49:21 -0400

    Add 64bit build of libcap2
    Signed-off-by: AltimatOS Build System <builds@altimatos.org>

---
+++ pkgs/40-libcap-64bit/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+set -x
+
+function compress_manpages {
+    local section=${1}
+    for FILE in $(find /System/share/man/man${section} -type f -name "*.${section}"); do
+        echo "Compressing file: $FILE"
+        gzip -f -9 $FILE
+    done
+}
+
+NAME=libcap
+VERSION=2.70
+SOURCE0="${NAME}-${VERSION}.tar.xz"
+SRC_DIR="${NAME}-${VERSION}"
+
+if [[ "$(pwd)" != "/srcs" ]]; then
+    cd /srcs
+fi
+
+# now unpack
+tar xvf $SOURCE0
+
+pushd $SRC_DIR
+    make prefix=/System lib=lib64
+    make test
+    make prefix=/System lib=lib64 install
+popd
+
+compress_manpages 1
+compress_manpages 3
+compress_manpages 5
+compress_manpages 8
+
+# clean up
+rm -rf $SRC_DIR
