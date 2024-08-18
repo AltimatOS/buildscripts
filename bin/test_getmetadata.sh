8507ffa0d48abdeb3e5aec0fd8edce79fd79daf9
Gary Greene <greeneg@tolharadys.net>
2024-08-17 20:11:16 -0400

    Rework functions to account for multiple packages defined in a blueprint
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- bin/test_getmetadata.sh
+++ bin/test_getmetadata.sh
@@ -19,27 +19,32 @@ PATH="$SCRIPT_BIN_DIR:/bin:/usr/bin"
 
 BP_FILE=${1}
 
-printf "%-16s %s\n" "Name:" $(get_pkgname $BP_FILE)
-printf "%-16s %s\n" "Epoch:" $(get_pkg_epoch $BP_FILE)
-printf "%-16s %s\n" "Version:" $(get_pkgversion $BP_FILE)
-printf "%-16s %s\n" "License:" $(get_license $BP_FILE)
-printf "%-16s %s\n" "URL:" $(get_srcurl $BP_FILE)
-printf "%-16s %s\n" "Section:" $(get_section $BP_FILE)
-printf "%-16s %s\n" "Summary:" $(get_summary $BP_FILE)
-for DEP in $(get_pkg_requires $BP_FILE); do
-    printf "%-16s %s\n" "Requires:" "$DEP"
+declare -a pkgs=$(get_packages $BP_FILE)
+
+for pkg in $(echo $pkgs); do
+    printf "%-16s %s\n" "Name:" $(get_pkg_name $pkg $BP_FILE)
+    printf "%-16s %s\n" "Epoch:" $(get_pkg_epoch $pkg $BP_FILE)
+    printf "%-16s %s\n" "Version:" $(get_pkg_version $pkg $BP_FILE)
+    printf "%-16s %s\n" "License:" $(get_pkg_license $pkg $BP_FILE)
+    printf "%-16s %s\n" "URL:" $(get_bld_srcurl $BP_FILE)
+    printf "%-16s %s\n" "Section:" $(get_pkg_section $pkg $BP_FILE)
+    printf "%-16s %s\n" "Summary:" "$(get_pkg_summary $pkg $BP_FILE)"
+    for DEP in $(get_pkg_requires $pkg $BP_FILE); do
+        printf "%-16s %s\n" "Requires:" "$DEP"
+    done
+    for DEP in $(get_build_requires $BP_FILE); do
+        printf "%-16s %s\n" "BuildRequires:" "$DEP"
+    done
+    src_instance=0
+    for SRC in $(get_build_srcs $BP_FILE); do
+        printf "%-16s %s\n" "Source${src_instance}:" "$SRC"
+        ((++src_instance))
+    done
+    patch_instance=0
+    for PATCH in $(get_build_patches $BP_FILE); do
+        printf "%-16s %s\n" "Patch${patch_instance}:" "$PATCH"
+        ((++patch_instance))
+    done
+    say "Description:" && get_pkg_description $pkg $BP_FILE
+    echo
 done
-for DEP in $(get_pkg_bldrequires $BP_FILE); do
-    printf "%-16s %s\n" "BuildRequires:" "$DEP" 
-done
-src_instance=0
-for SRC in $(get_srcs $BP_FILE); do
-    printf "%-16s %s\n" "Source${src_instance}:" "$SRC"
-    ((++src_instance))
-done
-patch_instance=0
-for PATCH in $(get_patches $BP_FILE); do
-    printf "%-16s %s\n" "Patch${patch_instance}:" "$PATCH"
-    ((++patch_instance))
-done
-say "Description:" && get_description $BP_FILE
